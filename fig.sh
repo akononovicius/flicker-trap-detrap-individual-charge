d7a12f0dcf144ca4849a5c7d315bf3f6cb7539b2
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2024-10-04 20:02:04 +0300

    fig.sh: enclose subshell

--- fig.sh
+++ fig.sh
@@ -4,7 +4,7 @@ for fig_file in fig_*.py; do
     python3 "$fig_file"
 done
 
-if [ -n $( command -v "gs" ) ]; then
+if [ -n "$( command -v "gs" )" ]; then
     # if ghostscript is installed, convert fonts to paths in PDF files
     find figs/ -maxdepth 1 -name "*.pdf" \
         -exec gs -q -o {}.opt -dNoOutputFonts -sPAPERSIZE=a4 -sDEVICE=pdfwrite {} \; \
